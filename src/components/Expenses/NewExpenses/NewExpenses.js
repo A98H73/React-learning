a864471e0428a56c60f3ff5822ded61683f7da68
A98H73 <singh002abhijeet@gmail.com>
2022-07-06 10:27:22 +0530

    Form Part

--- src/components/Expenses/NewExpenses/NewExpenses.js
+++ src/components/Expenses/NewExpenses/NewExpenses.js
@@ -3,9 +3,14 @@ import React from "react";
 import ExpenseForm from "./ExpenseForm";
 
 const NewExpenses = () => {
+  const saveExpenseDataHandlerData = (enteredExpenseData) => {
+    const ExpenseData = { ...enteredExpenseData, id: Math.random().toString() };
+  };
   return (
     <div className="new-expense">
-      <ExpenseForm />
+      {/* Below is custom component EXPENSEFORM and custom property "onSaveExpenseData" */}
+
+      <ExpenseForm onSaveExpenseData />
     </div>
   );
 };
